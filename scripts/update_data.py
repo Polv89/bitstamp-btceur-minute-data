fc261fc229ba98a77f3f650e0c33683743c2303d
Polv89 <polv89@gmail.com>
2026-05-14 12:16:35 +0200

    Change currency pair from BTC/USD to BTC/EUR
    Change currency pair from BTC/USD to BTC/EUR

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -7,9 +7,9 @@ import pandas as pd
 import requests
 
 # Configuration
-CURRENCY_PAIR = "btcusd"
-BULK_DATA_PATH = "data/historical/btcusd_bitstamp_1min_2012-2025.csv"
-DAILY_DATA_PATH = "data/updates/btcusd_bitstamp_1min_latest.csv"
+CURRENCY_PAIR = "btceur"
+BULK_DATA_PATH = "data/historical/btceur_bitstamp_1min_2012-2025.csv"
+DAILY_DATA_PATH = "data/updates/btceur_bitstamp_1min_latest.csv"
 COLUMN_NAMES = ["timestamp", "open", "high", "low", "close", "volume"]
 
 # Configure logging
